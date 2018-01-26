171c5cecf76f899fc9b1047006f754abb1877a2c
Gustavo García <gugadev@MacbookPro.local>
2018-01-26 15:42:29 -0500

    404 page

---
+++ client/src/screens/404.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h1>Ups, página no encontrada</h1>
+        <img src="/static/img/404.svg" alt="404">
+    </div>
+</template>
+
+<style scoped>
+    .container {
+        text-align: center;
+    }
+    h1 {
+        color: rgba(255,255,255,.8);
+        font-family: 'Pacifico', sans-serif;
+        text-align: center;
+    }
+    img {
+        margin: 0 auto;
+        max-height: 380px;
+    }
+</style>
